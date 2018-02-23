ccc12669b37283e672aadbdcdc0229db93b049ea
julio cesar de menezes <yjmenezes@hotmail.com>
2018-02-23 15:50:42 -0300

    Update linhas_paralelas.py

--- python/paralelas/linhas_paralelas.py
+++ python/paralelas/linhas_paralelas.py
@@ -11,7 +11,7 @@ from os import putenv
 def usage():
     print """
     Script tosco mostrando como gerar paralelas de 1.45m usando Spatialite.
-    Precisa de um banco de dados trator.sqlite contendo a linha de referencia.
+    Precisa de um banco de dados paracatu.sqlite contendo a linha (LINESTRING) de referencia.
     """
 
 
