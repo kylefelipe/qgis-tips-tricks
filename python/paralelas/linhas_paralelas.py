4a480c4da30fbd382a4ebda7df123210d9065f67
Kyle Felipe <kylefelipe@gmail.com>
2018-02-23 16:52:20 -0300

    Update linhas_paralelas.py

--- python/paralelas/linhas_paralelas.py
+++ python/paralelas/linhas_paralelas.py
@@ -28,11 +28,7 @@ if __name__ == '__main__':
         print "SQLITE VERSION:", dados[0][0]
         print "SPATIALITE VERSION: ", dados[0][1]
 
-<<<<<<< HEAD
-        # Creating table linhas_paralelas to receive the new lines.
-=======
         # Creating table paracatu_paralelas to receive the new lines. 32723=WGS84 UTM 23S
->>>>>>> 4d25992e2001ff3fd36898e8f7404b38f6b66d9b
         con.executescript("""CREATE TABLE IF NOT EXISTS linhas_paralelas (id INTEGER PRIMARY KEY AUTOINCREMENT);
         SELECT AddGeometryColumn ('linhas_paralelas', 'geom', 32723, 'LINESTRING', 'XY');""")
         con.commit()
