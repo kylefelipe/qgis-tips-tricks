d4845d2de24c28a3838f35b9ced8524ee2a15a58
kylefelipe <kylefelipe@gmail.com>
2018-02-27 03:31:21 -0300

    Inserindo manipulação e inserção de CSV

--- python/postgis/config.py
+++ python/postgis/config.py
@@ -2,7 +2,7 @@
 from configparser import ConfigParser
  
  
-def config(filename='database.ini', section='postgresql'):
+def config_db(filename='database.ini', section='postgresql'):
     # create a parser
     parser = ConfigParser()
     # read config file
@@ -18,3 +18,41 @@ def config(filename='database.ini', section='postgresql'):
         raise Exception('Section {0} not found in the {1} file'.format(section, filename))
  
     return db
+
+
+def config_csv(filename='database.ini', section='file_csv'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    file_csv = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            file_csv[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return file_csv
+
+
+def config_table(filename='database.ini', section='table_csv'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    table = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            table[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+
+    return table
+
+
--- python/postgis/connect.py
+++ python/postgis/connect.py
@@ -1,8 +1,25 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # http://www.postgresqltutorial.com/postgresql-python/connect/
+# Author: Julio Menezes
+# Mod by: Kyle Felipe
 # sudo apt-cache search psycopg2
+# sudo apt-get install python3-psycopg2
+
 import psycopg2
-from config import config
+import csv
+import config
+
+csv_params = config.config_csv()
+table_params = config.config_table()
+
+
+def open_csv():
+    """ Open the CSV file"""
+    with open(file=csv_params["path"], encoding="utf8", mode="r", newline='') as csv_file:
+        rows = list(csv.reader(csv_file, delimiter=csv_params["delimiter"]))
+        heading = rows[0]
+        sql = str.join(" text, ", heading)
+        return heading, sql
 
 
 def connect():
@@ -10,12 +27,12 @@ def connect():
     conn = None
     try:
         # read connection parameters
-        params = config()
+        params = config.config_db()
  
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
         conn = psycopg2.connect(**params)
- 
+
         # create a cursor
         cur = conn.cursor()
         
@@ -27,12 +44,30 @@ def connect():
         db_version = cur.fetchone()
         print(db_version)
         
-        cur.execute('SELECT count(*) from "Limites_Estaduais_GCS";')
+        cur.execute('SELECT count(*) from shapes_basicos."Limites_Municipais_2016_jun";')
         retorno = cur.fetchone()
         print(retorno)
 
+        # Creating tabel acording to database.ini [table] and importing a csv file.
+
+        heading, data = open_csv()
+        print("Creating tabel {}...".format(table_params["table"]))
+        sql_create = ("""CREATE TABLE {schema}.{table} (id bigserial, {columns} text);"""
+            .format(schema=table_params["schema"], table=table_params["table"], columns=open_csv()[1]))
+        cur.execute(sql_create)
+
+        # Importing data from CSV
+        print("Importing CSV file...")
+        sql_copy = ("""COPY {schema}.{table} ({columns})
+        FROM '{path}' WITH (FORMAT CSV, HEADER, DELIMITER '{delim}');"""
+                    .format(schema=table_params["schema"], table=table_params["table"],
+                            columns=str.join(" ,",open_csv()[0]), path=csv_params["path"], delim=csv_params["delimiter"]))
+
+        cur.execute(sql_copy)
+
         # close the communication with the PostgreSQL
         cur.close()
+        conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
