b04c73e58f88edbaf244ff917f1467efadd31bee
Kyle <kylefelipe@gmail.com>
2018-06-04 02:56:37 -0300

    new way to get the feature's id

--- python/scripts/save_selected/save_selected.py
+++ python/scripts/save_selected/save_selected.py
@@ -11,31 +11,25 @@ node_group = root.insertGroup(0, "DADOS_FILTRADOS")
 def where(layer, exp):
     """Font https://docs.qgis.org/2.18/pt_BR/docs/pyqgis_developer_cookbook/
     expressions.html#expressions"""
+    layers = list(layer.getFeatures())
+    id = []
     if exp.hasParserError():
         raise Exception(exp.parserErrorString())
     exp.prepare(layer.pendingFields())
-    for feature in layer.getFeatures():
+    for feature in layers:
         value = exp.evaluate(feature)
         if exp.hasEvalError():
             raise ValueError(exp.evalErrorString())
         if bool(value):
-            yield feature
-
-
-def get_ids(layer):
-    """Get the ids that match with the expression"""
-    ids = []
-    # Getting feature´s id
-    for f in where(layer, exp):
-        ids.append(f[0])
-    return ids
+            id.append(layers.index(feature))
+    return id
 
 
 for layer in layers:
     # Select features in layer
     # cleaning previous selection
     layer.setSelectedFeatures([])
-    layer.setSelectedFeatures(get_ids(layer))
+    layer.setSelectedFeatures(where(layer, exp))
     folder_path = str(layer.source()).split(sep)[:-1]
     file = layer.name()+"_DadoFiltrado"
     layer_out = sep.join(folder_path)+sep+file
@@ -43,8 +37,8 @@ for layer in layers:
     if layerType == QgsMapLayer.VectorLayer:
         QgsVectorFileWriter.writeAsVectorFormat(layer,
                                                 r"{out}".format(out=layer_out),
-                                                "utf-8", None, "ESRI Shapefile",
-                                                True)
+                                                "utf-8", None, "ESRI Shapefile"
+                                                , True)
         new_layers.append(layer_out)
         # print r"{saida}".format(saida=file)
 
